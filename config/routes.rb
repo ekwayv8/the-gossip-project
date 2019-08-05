142a80320ca153706c4e5fb702d0bbd8150b9d65
ekwayv8 <yvon.ekwalla@gmail.com>
2019-08-05 17:47:02 +0200

    Création des pages de présentation de la team et de contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'static_pages/team'
+  get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Chemin vers la page d'acceuil
+  get 'static_pages/home'
+  # Chemin d'accès vers la page statique de présentation de la team
+  get 'static_pages/team', to: 'static_pages#team'
+  # Chemin d'accès vers la page statique de Contact
+  get 'static_pages/contact', to: 'static_pages#contact'
+
+
+
 end
