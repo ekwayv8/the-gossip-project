14b4ebd308d0ef110c8b70dff65508d2a0074012
ekwayv8 <yvon.ekwalla@gmail.com>
2019-08-05 15:33:06 +0200

    First commit

---
+++ app/models/gossip.rb
@@ -0,0 +1,6 @@
+class Gossip < ApplicationRecord
+  has_many :join_table_gossips_tags
+  has_many :tags, through: :join_table_gossips_tags
+  has_many :comments, as: :commentable
+  belongs_to :user
+end
