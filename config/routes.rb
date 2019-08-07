d7c0f5cb241e46cfba152795588c8b298c62e187
ekwayv8 <yvon.ekwalla@gmail.com>
2019-08-07 10:46:56 +0200

    MAJ des views avant création des routes auto

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   
-   
+
   #Chemin vers la page d'acceuil qui retourne tous les gossips (nom de l'auteur et le titre de ces derniers)
   get '/home', to: 'home#index'
   root 'home#index'
 
+  #Chemin permettant d'afficher la page d'un gossip à l'aide de son id
+  get 'gossip/:id', to: 'gossip#display_gossip', as:'gossip'
   # Chemin d'accès vers la page statique de présentation de la team
   get '/team', to: 'static_pages#team'
   # Chemin d'accès vers la page statique de Contact
@@ -13,8 +15,7 @@ Rails.application.routes.draw do
   # Chemin d'accès vers la landing page de Bienvenue personnalisée
   get 'welcome/:first_name', to: 'welcome#first_name'
 
-  #Chemin permettant d'afficher la page d'un gossip à l'aide de son id
-  get 'gossip/:id', to: 'gossip#display_gossip', as:'gossip'
+
 
 
 end
